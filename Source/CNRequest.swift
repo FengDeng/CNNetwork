1fe0ba6e7d00abb234261d5f98710bf9b41e1f37
邓锋 <704292743@qq.com>
2018-06-22 09:51:51 +0800

    update

--- Source/CNRequest.swift
+++ Source/CNRequest.swift
@@ -22,7 +22,7 @@ open class CNRequest<T:Codable> : CNRequestBase{
     private var publish = PublishSubject<CNResponse<T>>.init()
     
     open func subscribe(success:@escaping (T)->Void,failure:@escaping (Error)->Void)->Disposable{
-        return self.publish.subscribe(onNext: { (response) in
+        return self.publish.observeOn(MainScheduler.instance).subscribe(onNext: { (response) in
             switch response{
             case .success(value: let value):
                 success(value)
@@ -115,68 +115,3 @@ public extension CNRequest where T : Pageble{
     }
 
 }
-
-
-/*
-open class CNRequestArray<T:Codable> : CNRequestBase{
-    
-    open var page = 1 //翻页的page 和 size
-    open var size = 20 //此参数无效 目前服务器想返回几条 就返回几条，直接根据返回结果是不是0来判断hasNext
-    open var hasNext = false //是否有下一页 如果正在请求中 该参数也为false
-    
-    private var publish = PublishSubject<CNResponse<[T]>>.init()
-    
-    open func subscribe(success:@escaping ([T])->Void,failure:@escaping (Error)->Void)->Disposable{
-        return self.publish.subscribe(onNext: { (response) in
-            switch response{
-            case .success(value: let value):
-                success(value)
-                break
-            case .failure(error: let e):
-                failure(e)
-                break
-            }
-        })
-    }
-    
-    open func fetchFirst(){
-        self.hasNext = false
-        self.page = 1
-        self.fetch()
-    }
-    
-    open func fetchNext(){
-        if !self.hasNext{return}
-        self.hasNext = false //
-        self.page = self.page + 1
-        self.fetch()
-    }
-    
-    //override
-    
-    open override func shouldSend() -> Bool {
-        self.defaultParameters["page"] = self.page
-        return true
-    }
-    
-    open override func didReceiveData(responseData: Data) {
-        super.didReceiveData(responseData: responseData)
-        do {
-            let decoder = JSONDecoder()
-            let obj = try decoder.decode([T].self, from: responseData)
-            if obj.count != 0{
-                //结束不为0  说明有下一页
-                self.hasNext = true
-            }
-            self.publish.onNext(CNResponse.success(value: obj))
-        } catch  {
-            self.hasNext = true
-            self.publish.onNext(CNResponse.failure(error: error as NSError))
-        }
-    }
-    open override func receiveError(error: NSError) {
-        self.hasNext = true
-        self.publish.onNext(CNResponse.failure(error: error as NSError))
-    }
-}
-*/
